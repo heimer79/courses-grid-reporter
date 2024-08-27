464206d0e55a2192131c91e0387b643c555578c0
Heimer <heimer79@gmail.com>
2024-08-27 14:58:09 -0500

    init repo

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '796b38692230d2a27e14');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '32257398867f97dfbe39');
